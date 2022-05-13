c5b880251a033b002be083857cca38c414d787b5
aromit_jhs <jungcomet9@gmail.com>
2022-05-13 16:48:51 +0900

    quiz : 03-3

--- quiz/03-3/sample/js/app.js
+++ quiz/03-3/sample/js/app.js
@@ -2,6 +2,12 @@
 // sibling-component 를 이름으로 갖는 새로운 컴포넌트를 아래에 등록해보세요.
 // options : template, props
 
+Vue.component('sibling-component', {
+  template: '<p>{{ propsdata }}</p>',
+  props: ['propsdata'],
+});
+
+
 Vue.component('child-component', {
   props: ['propsdata'],
   template: '<p>{{ propsdata }}</p>'
@@ -12,6 +18,7 @@ var app = new Vue({
   data: {
     message: 'Hello Vue! passed from Parent Component',
 
+    anotherMessage: '전역 컴포넌트에서 전달하는 메시지'
     // 할일 #2
     // data 속성을 1개 더 지정하고 (예: anotherMessage) 임의의 문자열을 값으로 대입해보세요.
     // 새로 지정한 data 속성은 위 sibling-component에 props로 전달합니다.
